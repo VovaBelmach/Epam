e3b93d0ff69642e2b87205774e66463517cba881
VovaBelmach\Владимир Бельмач <vova.belmach@mail.ru>
2017-10-27 22:07:37 +0300

    Lab_4
    Lab_4

---
+++ UnitTestProject3/UnitTestProject3/UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Chrome;
+
+namespace UnitTestProject3
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        ChromeDriver chromeDriver;
+        private const string baseurl = "https://github.com/";
+        private const string username = "testautomationuser";
+        private const string password = "Time4Death!";
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            chromeDriver = new ChromeDriver();
+            chromeDriver.Navigate().GoToUrl(baseurl);
+            chromeDriver.FindElementByXPath(".//a[text() = 'Sign in']").Click();
+            chromeDriver.FindElementById("login_field").SendKeys(username);
+            chromeDriver.FindElementById("password").SendKeys(password);
+        }
+        [TestCleanup]
+        public void TearDown()
+        {
+            chromeDriver.Quit();
+        }
+    }
+}
