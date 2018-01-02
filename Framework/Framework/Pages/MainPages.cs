ac6ecdf75d5b663c171360e1246362ff84a04f4f
VovaBelmach\Владимир Бельмач <vova.belmach@mail.ru>
2018-01-02 23:18:30 +0300

    TestFramework

---
+++ Framework/Framework/Pages/MainPages.cs
@@ -0,0 +1,344 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using OpenQA.Selenium.Interactions;
+
+namespace Framework.Pages
+{
+    class MainPages
+    {
+        private const string BASE_URL = "https://www.aircanada.com/ca/en/aco/home.html";
+        private IWebDriver driver;
+
+        int i;
+
+        [FindsBy(How = How.Id, Using = "origin_focus_0")]
+        private IWebElement clickFrom;
+
+        [FindsBy(How = How.Id, Using = "origin_0")]
+        private IWebElement inputFrom;
+
+        [FindsBy(How = How.Id, Using = "flightLocationListOrginId0_locationListItem_0")]
+        private IWebElement listFrom;
+
+        [FindsBy(How = How.Id, Using = "destination_label_0")]
+        private IWebElement clickTo;
+
+        [FindsBy(How = How.Id, Using = "destination_0")]
+        private IWebElement inputTo;
+
+        [FindsBy(How = How.Id, Using = "flightLocationListDestinationId0_locationListItem_0")]
+        private IWebElement listTo;
+
+        [FindsBy(How = How.Id, Using = "departureDateLabel")]
+        private IWebElement blockDepart;
+
+        [FindsBy(How = How.Id, Using = "fligthDepartureDate")]
+        private IWebElement dateBlockDepart;
+
+        [FindsBy(How = How.Id, Using = "returnDateLabel")]
+        private IWebElement blockReturn;
+
+        [FindsBy(How = How.Id, Using = "fligthReturnDate")]
+        private IWebElement dateBlockReturn;
+
+        [FindsBy(How = How.Id, Using = "passengersInputLabel")]
+        private IWebElement inputPassenger;
+
+        [FindsBy(How = How.Id, Using = "btnAdultCountSub")]
+        private IWebElement removeAdult;
+
+        [FindsBy(How = How.Id, Using = "btnChildCountAdd")]
+        private IWebElement inputChild;
+
+        [FindsBy(How = How.Id, Using = "btnAdultCountAdd")]
+        private IWebElement inputAdult;
+
+        [FindsBy(How = How.Id, Using = "btnYouthCountAdd")]
+        private IWebElement inputYouths;
+
+        [FindsBy(How = How.Id, Using = "btnInfantCountAdd")]
+        private IWebElement inputInfants;
+
+        [FindsBy(How = How.Id, Using = "oneWay")]
+        private IWebElement inputOneWay;
+
+        [FindsBy(How = How.Id, Using = "multiCity")]
+        private IWebElement inputMultiCity;
+
+        [FindsBy(How = How.Id, Using = "btnRemoveTrip")]
+        private IWebElement removeTrip;
+
+        [FindsBy(How = How.XPath, Using = "//*[@id='bookingMagnetForm']/fieldset/div[3]/div[1]/div[3]/div[1]")]
+        private IWebElement blockDepartMultiCity;
+
+        [FindsBy(How = How.Id, Using = "departure_0")]
+        private IWebElement departMultiCity;
+
+        [FindsBy(How = How.Id, Using = "origin_1")]
+        private IWebElement inputFromMultiCity;
+
+        [FindsBy(How = How.Id, Using = "flightLocationListOrginId1")]
+        private IWebElement listFromMultiCity;
+
+        [FindsBy(How = How.Id, Using = "origin_focus_1")]
+        private IWebElement clickFromMultiCity;
+
+        [FindsBy(How = How.Id, Using = "destination_1")]
+        private IWebElement inputToMultiCity;
+
+        [FindsBy(How = How.Id, Using = "flightLocationListDestinationId1")]
+        private IWebElement listToMultiCity;
+
+        [FindsBy(How = How.Id, Using = "destination_label_1")]
+        private IWebElement clickToMultiCity;
+
+        [FindsBy(How = How.XPath, Using = "//*[@id='bookingMagnetForm']/fieldset/div[3]/div[2]/div[3]/div[1]")]
+        private IWebElement toBlockDepartMultiCity;
+
+        [FindsBy(How = How.Id, Using = "departure_1")]
+        private IWebElement toDepartMultiCity;
+
+        [FindsBy(How = How.Id, Using = "enCAEdition")]
+        private IWebElement selectLanguage;
+
+        [FindsBy(How = How.XPath, Using = "//input[@class='search-active-magnet btn btn-primary']")]
+        private IWebElement buttonSubmit;
+
+        [FindsBy(How = How.XPath, Using = "//*[@id='bookingMagnetNtpErrorId_0']/div")]
+        private IWebElement errorAirport;
+
+        [FindsBy(How = How.XPath, Using = "//*[@id='bookingMagnetPopupErrorId_0']/div")]
+        private IWebElement errorDateReturn;
+
+        [FindsBy(How = How.Id, Using = "childErrorContentId")]
+        private IWebElement errorNameAirport;
+
+        [FindsBy(How = How.XPath, Using = "//*[@id='fligthPaxErrorId_0']")]
+        private IWebElement errorPassenger;
+
+        [FindsBy(How = How.XPath, Using = "//*[@id='fligthPaxErrorId_0']")]
+        private IWebElement errorChildren;
+
+        [FindsBy(How = How.XPath, Using = "//*[@id='fligthPaxErrorId_0']")]
+        private IWebElement errorInfants;
+
+        [FindsBy(How = How.XPath, Using = "//*[@id='bookingMagnetNtpErrorId_0']/div/div")]
+        private IWebElement errorDateDeparn;
+
+        [FindsBy(How = How.XPath, Using = "//*[@id='bookingMagnetPopupErrorId_0']")]
+        private IWebElement errorFareType;
+
+        [FindsBy(How = How.XPath, Using = "//*[@id='bookingMagnetNtpErrorId_0']/div/div")]
+        private IWebElement errorMultiCity;
+
+        [FindsBy(How = How.XPath, Using = "//*[@id='bookingMagnetPopupErrorId_0']/div[1]")]
+        private IWebElement errorBookingInfants;
+
+        public MainPages(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(this.driver, this);
+        }
+
+        public void OpenPage()
+        {
+            driver.Navigate().GoToUrl(BASE_URL);
+        }
+
+        public void ButtonSearch()
+        {
+            IJavaScriptExecutor ex = (IJavaScriptExecutor)driver;
+            ex.ExecuteScript("arguments[0].click();", buttonSubmit);
+        }
+
+        public void SelectLanguage()
+        {
+            selectLanguage.Click();
+        }
+
+        public void FillingFrom(string from)
+        {
+            clickFrom.Click();
+            inputFrom.SendKeys(from);
+            listFrom.Click();
+        }
+
+        public void FillingFromInvalid(string from)
+        {
+            clickFrom.Click();
+            inputFrom.SendKeys(from);
+        }
+
+        public void FillingTo(string to)
+        {
+            clickTo.Click();
+            inputTo.SendKeys(to);
+            listTo.Click();
+        }
+
+        public void FillingDepartDate(string departDate)
+        {
+            blockDepart.Click();
+            dateBlockDepart.SendKeys(departDate);
+        }
+
+        public void FillingReturnDate(string returnDate)
+        {
+            blockReturn.Click();
+            dateBlockReturn.SendKeys(returnDate);
+        }
+
+        public void FillingInvalidChildrenSending()
+        {
+            inputPassenger.Click();
+            removeAdult.Click();
+            inputChild.Click();
+        }
+
+        public void FillingPassengers()
+        {
+            inputPassenger.Click();
+        }
+
+        public void InputChildren()
+        {
+            for (i = 0; i < 9; i++)
+            {
+                inputChild.Click();
+            }
+        }
+
+        public void InputInfants()
+        {
+            for (i = 0; i < 2; i++)
+            {
+                inputInfants.Click();
+            }
+        }
+
+        public void ChooseOneWay()
+        {
+            inputOneWay.Click();
+        }
+
+        public void InputYesterdayDepartDay()
+        {
+            blockDepart.Click();
+            dateBlockDepart.SendKeys(Keys.Left);
+            dateBlockDepart.SendKeys(Keys.Enter);
+        }
+
+        public void ChooseMultiCity()
+        {
+            inputMultiCity.Click();
+        }
+
+        public void RemoveTrip()
+        {
+            removeTrip.Click();
+        }
+
+        public void FillingMultiCityDepartDate(string departDate)
+        {
+            blockDepartMultiCity.Click();
+            departMultiCity.SendKeys(departDate);
+        }
+
+        public void FillingMulticityFrom(string to)
+        {
+            clickFromMultiCity.Click();
+            inputFromMultiCity.SendKeys(to);
+            listFromMultiCity.Click();
+        }
+
+        public void FillingMulticityTo(string from)
+        {
+            clickToMultiCity.Click();
+            inputToMultiCity.SendKeys(from);
+            listToMultiCity.Click();
+        }
+
+        public void FillingMultiCityDepartDateReturn(string departDate)
+        {
+            toBlockDepartMultiCity.Click();
+            toDepartMultiCity.SendKeys(departDate);
+        }
+
+        public void FillingReturnMultiCityDepartDate(string returnDate)
+        {
+            toBlockDepartMultiCity.Click();
+            toDepartMultiCity.SendKeys(returnDate);
+        }
+
+        public void InputPassenger()
+        {
+            inputAdult.Click();
+
+            for (i = 0; i < 3; i++)
+            {
+                inputYouths.Click();
+            }
+
+            for (i = 0; i < 2; i++)
+            {
+                inputChild.Click();
+            }
+
+            for (i = 0; i < 2; i++)
+            {
+                inputInfants.Click();
+            }
+        }
+
+        public bool GetErrorAirport(string error)
+        {
+            return errorAirport.Text == error;
+        }
+
+        public bool GetErrorDate(string error)
+        {
+            return errorDateReturn.Text == error;
+        }
+
+        public bool GetErrorNameAirport(string error)
+        {
+            return errorNameAirport.Text == error;
+        }
+
+        public bool GetErrorPassenger(string error)
+        {
+            return errorPassenger.Text == error;
+        }
+
+        public bool GetErrorChildren(string error)
+        {
+            return errorChildren.Text.Contains(error);
+        }
+
+        public bool GetErrorInfants(string error)
+        {
+            return errorInfants.Text == error;
+        }
+
+        public bool GetErrorDateDepart(string error)
+        {
+            return errorDateDeparn.Text == error;
+        }
+
+        public bool GetErrorFareType(string error)
+        {
+            return errorFareType.Text == error;
+        }
+
+        public bool GetErrorMultiCity(string error)
+        {
+            return errorMultiCity.Text == error;
+        }
+
+        public bool GetErrorBookingInfants(string error)
+        {
+            return errorBookingInfants.Text.Contains(error);
+        }
+    }
+}
---
+++ Framework/Framework/Tests/Tests.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Framework.Tests
+{
+    class Tests
+    {
+        private static Steps.Steps steps = new Steps.Steps();
+        string from = "Calgary";
+        string invalidFrom = "Calgaryy";
+        string to = "Moscow";
+        string departDate = DateTime.Today.ToShortDateString();
+        string returnDate = DateTime.Today.AddDays(4).ToShortDateString();
+        string returnDateEmpty = string.Empty;
+        string yesterday = DateTime.Today.AddDays(-1).ToShortDateString();
+
+        private const string missing_airport_error_message = "The departure and arrival airports you've selected " +
+            "are one and the same. Please review your selection and try again.";
+        private const string missing_airport_date_return_message = "The dates you've selected don't appear to be " +
+            "valid. Please make any necessary corrections and try your search again.";
+        private const string airporn_name_error_message = "We can't seem to locate the point of origin you've entered. " +
+            "Please try again. Note: When you enter a country, state, province or city name in the search field, " +
+            "a choice of airports will be displayed.";
+        private const string missing_passenger_message = "Children under age 8 must travel with an adult age 16 or older." +
+            " Children ages 8 to 11 can travel alone but you'll need to contact Air Canada Reservations at least 48 " +
+            "hours before departure to purchase their ticket and request our Unaccompanied Minor service.";
+        private const string missing_children_message = "You have selected more than 9 passengers.";
+        private const string missing_infants_message = "Only one infant per adult is permitted. You have " +
+            "indicated that 2 children are travelling with 1 adult(s). Please make any necessary corrections " +
+            "before proceeding.";
+        private const string missing_airport_date_depart_message = "The departure date/time has to be at least" +
+            " 2 hours before departure. Please verify your entry and try again.";
+        private const string missing_fary_type_message = "Please select a fare type from the drop-down " +
+            "menu located directly beneath your flights.";
+        private const string missing_multicity_message = "The return date/time has to be at least 2 hours " +
+            "before departure. Please verify your entry and try again.";
+        private const string missing_booking_infants_message = "To include an infant in your booking, please call " +
+            "Air Canada Reservations after you've booked your flight online (this applies to international flights as " +
+            "well as travel to/from the U.S).";
+
+        [SetUp]
+        public void Init()
+        {
+            steps.InitBrowser();
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            steps.CloseBrowser();
+        }
+
+        [Test]
+        public void TestInvalidArrivalAirport()
+        {
+            steps.SearchCaseOne(from, from, departDate, returnDate);
+            Assert.IsTrue(steps.GetErrorAirport(missing_airport_error_message));
+        }
+
+        [Test]
+        public void TestInvalidReturnDate()
+        {
+            steps.SearchCaseTwo(from, to, departDate, returnDateEmpty);
+            Assert.IsTrue(steps.GetErrorDate(missing_airport_date_return_message));
+        }
+
+        [Test]
+        public void TestInvalidAirport()
+        {
+            steps.SearchCaseThree(invalidFrom, to, departDate, returnDate);
+            Assert.IsTrue(steps.GetErrorNameAirport(airporn_name_error_message));
+        }
+
+        [Test]
+        public void TestInvalidChildrenSending()
+        {
+            steps.SearchCaseFour(from, to, departDate, returnDate);
+            Assert.IsTrue(steps.GetErrorPassenger(missing_passenger_message));
+        }
+
+        [Test]
+        public void TestInvalidNumberOfPassengers()
+        {
+            steps.SearchCaseFive(from, to, departDate, returnDate);
+            Assert.IsTrue(steps.GetErrorChildren(missing_children_message));
+        }
+
+        [Test]
+        public void TestInvalidNumberOfInfantsPerAdult()
+        {
+            steps.SearchCaseSex(from, to, departDate, returnDate);
+            Assert.IsTrue(steps.GetErrorInfants(missing_infants_message));
+        }
+
+        [Test]
+        public void TestInvalidDepartDate()
+        {
+            steps.SearchCaseSeven(from, to);
+            Assert.IsTrue(steps.GetErrorDateDepart(missing_airport_date_depart_message));
+        }
+
+        [Test]
+        public void TestInvalidFareType()
+        {
+            steps.SearchCaseEight(from, to, departDate);
+            Assert.IsTrue(steps.GetErrorFareType(missing_fary_type_message));
+        }
+
+        [Test]
+        public void TestInvalidDate()
+        {
+            steps.SearchCaseNine(from, to, departDate);
+            Assert.IsTrue(steps.GetErrorMultiCity(missing_multicity_message));
+        }
+
+        [Test]
+        public void TestIncludeInfants()
+        {
+            steps.SearchCaseTen(from, to, departDate, returnDate);
+            Assert.IsTrue(steps.GetErrorBookingInfants(missing_booking_infants_message));
+        }
+    }
+}
